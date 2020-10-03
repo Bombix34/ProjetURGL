943163944be2446be3f8ea2a5071489f48082093
Sulexa <sulexa95@gmail.com>
2020-10-03 14:55:44 +0200

    Add multiplayer demo

---
+++ Assets/Scripts/IAMovement.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class IAMovement : NetworkBehaviour
+{
+    public Vector2 speed = new Vector2(50, 50);
+
+    private void Update()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        float inputX = UnityEngine.Random.Range(-1f, 1f);
+        float inputY = UnityEngine.Random.Range(-1f, 1f);
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
