a83aad3751fac47fd0ec798df275286acbeba096
Hugo <bombix34@gmail.com>
2020-10-03 18:19:51 +0200

    debut comportement IA + passage online des IAs

---
+++ Assets/Scripts/PNJ/PNJManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PNJManager : ObjectManager
+{
+    public NavMeshAgent Agent { get; private set; }
+    public PNJPositionDatas PositionDatas { get; private set; }
+
+
+    private void Start()
+    {
+        if (!isServer)
+            return;
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.updateRotation = false;
+        Agent.updateUpAxis = false;
+        PositionDatas = FindObjectOfType<PNJPositionDatas>();
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        ChangeState(new PNJMoveState(this, PositionDatas.GetPosition()));
+    }
+
+    protected override void Update()
+    {
+        if (!isServer)
+            return;
+        if (currentState == null)
+            return;
+        currentState.Execute();
+    }
+}
---
+++ Assets/Scripts/PNJ/PNJMoveState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNJMoveState : PNJState
+{
+    private Vector2 target;
+
+    public PNJMoveState(ObjectManager curObject) : base(curObject)
+    {
+        stateName = "PNJ_MOVE";
+    }
+
+    public PNJMoveState(ObjectManager curObject, Vector2 targetPosition) : base(curObject)
+    {
+        stateName = "PNJ_MOVE";
+        manager = (PNJManager)curObject;
+        target = new Vector3(targetPosition.x + Random.Range(-0.6f,0.6f),targetPosition.y+Random.Range(-0.6f,0.6f),manager.transform.position.z);
+    }
+
+    #region STATE_METHODS
+
+    public override void Enter()
+    {
+        manager.Agent.SetDestination(target);
+    }
+
+    public override void Execute()
+    {
+        if (Vector3.Distance(manager.transform.position, target) < 0.3f)
+        {
+            manager.ChangeState(new PNJWaitState(manager));
+        }
+    }
+
+    public override void Exit()
+    {
+    }
+    #endregion
+}
