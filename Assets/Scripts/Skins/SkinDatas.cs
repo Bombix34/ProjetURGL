5d749fe313f6399f637c44d76b14d6db2079960e
Hugo <bombix34@gmail.com>
2020-10-04 18:45:09 +0200

    debut generation de skin simple

---
+++ Assets/Scripts/Skins/SkinDatas.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skins datas")]
+public class SkinDatas : ScriptableObject
+{
+    public List<Color> colors;
+    public List<Sprite> heads;
+}
---
+++ Assets/Scripts/Skins/SkinDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SkinDisplay : NetworkBehaviour
+{
+    [SerializeField]
+    private SkinDatas datas;
+
+    [SerializeField]
+    private SpriteRenderer bodySprite, headSprite;
+
+    public override void OnStartServer()
+    {
+        if(isServer || hasAuthority)
+            Generate();
+    }
+
+    private void Generate()
+    {
+        bodySprite.color = datas.colors[Random.Range(0, datas.colors.Count)];
+        headSprite.sprite = datas.heads[Random.Range(0, datas.heads.Count)];
+    }
+}
