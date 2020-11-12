1b2cd28a2fe7522a56c3a23b3a224183dac85698
Hugo <bombix34@gmail.com>
2020-11-12 11:51:10 +0100

    doors hidden working

---
+++ Assets/Scripts/FX/FogVisibilityRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogVisibilityRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fogRenderer, noFogRenderer;
+
+    private bool IsVisible = false;
+
+    public void SwitchVisibility(bool isVisible)
+    {
+        //PASSAGE A LETAT INVISIBLE
+        if (!isVisible && this.IsVisible)
+        {
+            fogRenderer.SetActive(true);
+            noFogRenderer.GetComponent<FogOfWarSprite>().enabled = true;
+            noFogRenderer.GetComponent<SpriteRenderer>().enabled = false;
+            this.IsVisible = isVisible;
+        }
+        //PASSAGE A LETAT VISIBLE
+        else if (isVisible && !this.IsVisible)
+        {
+            fogRenderer.SetActive(false);
+            noFogRenderer.GetComponent<FogOfWarSprite>().enabled = false;
+            noFogRenderer.GetComponent<SpriteRenderer>().enabled = true;
+
+            this.IsVisible = isVisible;
+        }
+    }
+}
