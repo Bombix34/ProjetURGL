91ea407f7bd8ee0c43696f351114f56aa44d2d2d
Sulexa <sulexa95@gmail.com>
2020-11-26 12:59:42 +0100

    Add item ui

---
+++ Assets/Scripts/UI/ItemUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ItemUI : MonoBehaviour
+{
+    public void Init(ItemScriptableObject item)
+    {
+        var spriteRenderer = GetComponent<Image>();
+        spriteRenderer.sprite = item.Sprite;
+    }
+}
